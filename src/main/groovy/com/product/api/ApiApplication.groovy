194ee1e2e9914df4dfa4b30dcffe8e4b8c63575f
Zachary Lockner <zacharylockner@zacharys-mbp.lan>
2019-08-28 19:10:43 -0500

    added main class and app properties

--- src/main/groovy/com/product/api/ApiApplication.groovy
+++ src/main/groovy/com/product/api/ApiApplication.groovy
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class ApiApplication {
+public class Application {
 
 	static void main(String[] args) {
-		SpringApplication.run(ApiApplication, args)
+		SpringApplication.run(Application.class, args)
 	}
 
 }
